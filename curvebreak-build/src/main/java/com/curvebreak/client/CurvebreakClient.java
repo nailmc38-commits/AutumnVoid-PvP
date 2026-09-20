d6fb60d196e4a1d13b2e60fb8b740ba862a9a101
nailmc38-commits <nailmc38@gmail.com>
2026-09-19 17:16:48 -0700

    Curvebreak CI: add client initializer

---
+++ curvebreak-build/src/main/java/com/curvebreak/client/CurvebreakClient.java
@@ -0,0 +1,9 @@
+package com.curvebreak.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CurvebreakClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
